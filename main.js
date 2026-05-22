ec510ec5db091101fe83104003a98cd841e96be5
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-05-22 23:38:25 +0530

    initial

---
+++ main.js
@@ -0,0 +1,14 @@
+/* backend express server creation
+import React from "react";
+import Express from "express"; */
+
+const express = require("express");
+const app = express();
+
+// listen -> express inbuild function -> it returns the http
+// 3000, 3001, 3002, 8080, 8081
+let port = 8080;
+
+app.listen(port, () => {
+    console.log("Server running on", port);
+});
